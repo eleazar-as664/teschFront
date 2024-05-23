98ace75cc2cbb4f0e513994824a9738e318d01fd
Jhonatan Victorio <jhonatan.victorio@certuslab.com.mx>
2024-05-23 15:48:13 -0700

    Eliminado

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,5 +1,5 @@
 // const BASE_URL_SERVER = "http://hanab1:3000/api/v1";
-const BASE_URL_SERVER = "http://localhost:3000/api/v1";
+
 const routes = {
   BASE_URL_SERVER: BASE_URL_SERVER,
   URL_ROOT: "/",
