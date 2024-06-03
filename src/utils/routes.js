64069f6325efa9dfde7ce4b16116fb439f10ca9f
Certus Laboratorio <96503981+certusrepo@users.noreply.github.com>
2024-06-03 10:41:03 -0700

    Cambios

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,5 +1,4 @@
 // const BASE_URL_SERVER = "http://hanab1:3000/api/v1";
-const BASE_URL_SERVER = "http://localhost:3000/api/v1";
 const routes = {
   BASE_URL_SERVER: BASE_URL_SERVER,
   URL_ROOT: "/",
